2d7c8907b48f9b2aaa27398e0abea17a1e065ec2
Lander Joshua Vitug <lander.vitug@gmail.com>
2025-04-21 22:49:02 -0500

    build: stripped down webpack template for building packages

--- src/fonts/README.md
+++
@@ -1,1 +0,0 @@
-Insert all fonts here
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,14 @@
 const { merge } = require("webpack-merge");
+const path = require("path");
 const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
   mode: "production",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    library: {
+      type: "commonjs-static"
+    }
+  },
 });
