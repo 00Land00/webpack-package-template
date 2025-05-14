843fd6998cefc79044981de68d856843d7ccee96
Lander Joshua Vitug <lander.vitug@gmail.com>
2025-05-13 21:39:51 -0500

    feat: add jest config

--- webpack.common.js
+++ webpack.common.js
@@ -2,6 +2,19 @@ const path = require("path");
 
 module.exports = {
   entry: "./src/js/index.js",
+  rules: [
+    {
+      test: /\.(?:js|mjs|cjs)$/,
+      exclude: /node_modules/,
+      use: {
+        loader: "babel-loader",
+        options: {
+          targets: "defaults",
+          presets: [["@babel/preset-env"]],
+        },
+      },
+    },
+  ],
   resolve: {
     alias: {
       js: path.resolve(__dirname, "src/js"),
